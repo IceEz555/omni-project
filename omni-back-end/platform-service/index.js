5c341fc7977e412b0c82dc5369683e71389bf2d2
Apivit Y. <120260881+IceEz555@users.noreply.github.com>
2026-01-21 11:06:58 +0700

    Register admin routes in server

--- omni-back-end/platform-service/index.js
+++ omni-back-end/platform-service/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
-
+import adminRoutes from './routes/adminRoutes.js';
 dotenv.config();
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
 // Basic Health Check
 app.get('/', (req, res) => {
